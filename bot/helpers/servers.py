9045de4a6d180f9e5cfe5022af8146a236a88edd
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-09-02 20:13:18 +0530

    Update servers.py

--- bot/helpers/servers.py
+++ bot/helpers/servers.py
@@ -95,6 +95,7 @@ async def upload_handler(client: CloudBot, message: CallbackQuery, callback_data
                     response = await switch_upload(file_path=file_path,size=fize,message=status,start=start)
                     link = response.media_link
                     link = f"{shorten_link(link)}"
+                    print(response.media_link,link)
                 elif callback_data.endswith('telegraph'):
                         try:
                           telegraph = Telegraph()
